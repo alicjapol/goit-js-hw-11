9d19893e3d4de939c28c4bc9f5d73f8bd089a248
alicjapol <alicjaxpoltorak@gmail.com>
2023-12-12 15:41:03 +0100

    img

--- src/index.js
+++ src/index.js
@@ -9,7 +9,27 @@ const loadMore = document.querySelector('.load-more');
 
 let page = 1;
 
-gallery.style.display = 'none';
+gallery.style.display = 'hidden';
+
+const fetchImg = async (currentPage) => {
+  try {
+    const searchParams = new URLSearchParams({
+      key: apiKey,
+      q: input.value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: currentPage,
+      per_page: 40
+    });const form = document.querySelector('#search-form');
+const input = document.querySelector('input');
+const apiKey = '41180761-f0899a94a2e54aea5b2403dd8';
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+
+let page = 1;
+
+gallery.style.visibility = 'hidden';
 
 const fetchImg = async (currentPage) => {
   try {
@@ -22,6 +42,63 @@ const fetchImg = async (currentPage) => {
       page: currentPage,
       per_page: 40
     });
+
+    const response = await axios.get(
+      `https://pixabay.com/api/?${searchParams}`
+    );
+    const images = response.data.hits; 
+
+    if (images.length === 0) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    return images.map(image => ({
+      webformatURL: image.webformatURL,
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
+      likes: image.likes,
+      views: image.views,
+      comments: image.comments,
+      downloads: image.downloads,
+    }));
+  } catch (error) {
+    throw error;
+  }
+};
+
+loadMore.addEventListener('click', () => {
+  fetchAndRenderImg(++page);
+});
+
+const fetchAndRenderImg = async (currentPage) => {
+  try {
+    const images = await fetchImg(currentPage);
+    clearGallery();
+
+    if (images.length > 0) {
+
+      images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.largeImageURL;
+        imgElement.alt = image.tags;
+        gallery.appendChild(imgElement);
+        gallery.style.visibility = 'visible';
+
+      });
+    } else {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('Sorry, there was an error fetching the images. Please try again later.')
+  }
+};
+
+const clearGallery = () => {
+  gallery.innerHTML = '';
+};
+
 
     const response = await axios.get(
       `https://pixabay.com/api/?${searchParams}`
